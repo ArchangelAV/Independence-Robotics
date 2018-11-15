8cc5de2a998fc43afb02096424e0a3a06fdd1d0b
ArchangelAV <45073089+ArchangelAV@users.noreply.github.com>
2018-11-15 09:28:02 -0600

    Add files via upload

---
+++ AutoCopy.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import java.util.Set;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@Disabled
+@Autonomous(name = "AutonomousCopy", group = "AutonomousCopy")
+public class AutoCopy extends LinearOpMode
+{
+/**This is a copy of the Autonomous OP mode on the phone
+If you need to make edits please remove the @Disabled line from this code and make
+any edits you may need. If you need a new copy of this OP mode Text me at
+(319)-327-0338 and leave your email. I will sent you a new copy of this OP mode 
+Good luck at the competition
+-Connor **/ 
+
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
+
+
+    @Override
+    public void runOpMode() {
+        double StartTime;
+        motor1 = hardwareMap.dcMotor.get("motor1");
+        motor2 = hardwareMap.dcMotor.get("motor2");
+        motor3 = hardwareMap.dcMotor.get("motor3");
+        motor4 = hardwareMap.dcMotor.get("motor4");
+        motor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        while (opModeIsActive());
+
+        telemetry.addData("M1 Power", motor1.getPower());
+        telemetry.addData("M2 Power", motor2.getPower());
+        telemetry.addData("M3 Power", motor3.getPower());
+        telemetry.addData("M4 Power", motor4.getPower());
+        // Motor power updates on driver station phone
+        telemetry.update();
+        waitForStart();
+        StartTime = getRuntime();
+        motor3.setPower(-1);
+        motor2.setPower(-1);
+        while (!(isStopRequested() || getRuntime() - StartTime > 1)) {
+
+
+
+            // Drive motors off
+        } } }
