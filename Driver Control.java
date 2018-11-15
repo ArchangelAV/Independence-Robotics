37fd464c4b6329f2ea02cba5f14f7ad85abf6977
ArchangelAV <45073089+ArchangelAV@users.noreply.github.com>
2018-11-15 09:46:33 -0600

    Update and rename DC6625.java to Driver Control.java

--- Driver Control.java
+++ Driver Control.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @Disabled
 @TeleOp(name = "Driver Control 6625", group = "Driver Control 6625")
-public class DC6625 extends LinearOpMode {
+public class Driver Control extends LinearOpMode {
 // To Use remove @Disabled from line 12 // 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor motor1;
